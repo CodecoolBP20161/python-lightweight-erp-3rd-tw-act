38f188d27a1448b0fe6026884c4b815a212ca9c2
Benedek Vörös <xbenedek01@gmail.com>
2016-06-06 17:43:15 +0200

    ui print_table OK

--- ui.py
+++ ui.py
@@ -9,8 +9,23 @@
 # |   1    |       fo       |    fps  |
 # \-----------------------------------/
 def print_table(table, title_list):
-
-    # your code
+    max_len_items = table[0][0]
+    for i in range(len(table)):
+        for j in range(len(title_list)):
+            if len(table[i][j]) > len(max_len_items):
+                max_len_items = table[i][j]
+    max_len_items = len(max_len_items)
+    max_len_titles = len(max(title_list, key=len))
+    max_len_all = max_len_items
+    if max_len_titles > max_len_items:
+        max_len_all = max_len_titles
+    dynamic_row_length = len(title_list)*(max_len_all+2)+(len(title_list)-2)
+    print("|", "-"*dynamic_row_length, "|")
+    print(("|{:^{a}}"*len(title_list)).format(*title_list, a=max_len_all+2), "|")
+    for i in range(len(table)):
+        print("|", "-"*dynamic_row_length, "|")
+        print(("|{:^{a}}"*len(title_list)).format(*table[i], a=max_len_all+2), "|")
+    print("|", "-"*dynamic_row_length, "|")
 
     pass
 
