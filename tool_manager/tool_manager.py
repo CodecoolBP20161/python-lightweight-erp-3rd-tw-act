27e4103bbe8cf139caaf9651e59aad9b42e78295
Benedek Vörös <xbenedek01@gmail.com>
2016-06-07 15:55:52 +0200

    submenu loop function

--- tool_manager/tool_manager.py
+++ tool_manager/tool_manager.py
@@ -22,26 +22,27 @@ common = SourceFileLoader("common", current_file_path + "/../common.py").load_mo
 # start this manager by a menu
 def start():
     options = ["Show table", "Add new item", "Remove item", "Update item"]
-    ui.print_menu("Accounting submenu", options, "Exit program")
-    inputs = ui.get_inputs(["Please choose an option: "], "")
-    option = inputs[0]
-    table = data_manager.get_table_from_file(current_file_path + "/tools.csv")
-    id_ = "0"
-    if option == "1":
-        show_table(table)
-    elif option == "2":
-        add(table)
-    elif option == "3":
-        remove(table, id_)
-    elif option == "4":
-        update(table, id_)
-    # elif option == 5:
-    # elif option == 6:
-    elif option == "0":
+    while True:
+        ui.print_menu("Accounting submenu", options, "Exit program")
+        inputs = ui.get_inputs(["Please choose an option: "], "")
+        option = inputs[0]
+        table = data_manager.get_table_from_file(current_file_path + "/tools.csv")
+        id_ = "0"
+        if option == "1":
+            show_table(table)
+        elif option == "2":
+            add(table)
+        elif option == "3":
+            remove(table, id_)
+        elif option == "4":
+            update(table, id_)
+        # elif option == 5:
+        # elif option == 6:
+        elif option == "0":
+            break
+        else:
+            raise KeyError("There is no such option.")
         pass
-    else:
-        raise KeyError("There is no such option.")
-    pass
 
 
 # print the default table of records from the file
