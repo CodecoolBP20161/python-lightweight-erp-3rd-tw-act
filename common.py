81a2967c26487501349d32c05f2ed0ed92d82a98
beru26 <berkes.edina@gmail.com>
2016-06-08 12:22:55 +0200

    common.py is OK

--- common.py
+++ common.py
@@ -1,5 +1,7 @@
 # implement commonly used functions here
 import random
+import ui
+import data_manager
 
 
 # generate and return a unique and random
@@ -28,3 +30,36 @@ def generate_random(table):
                 boolean_generate = False
         if boolean_generate:
             return password
+
+
+def comm_add(table, title_list):
+    temp_list = []
+    new_line = [generate_random(table)]
+    for i in range(len(title_list)):
+        title_list[0] = title_list[i]
+        temp_list.append(ui.get_inputs(title_list, "Give me a data!"))
+        new_line.append(temp_list[i][0])
+    table.append(new_line)
+    return table
+
+
+def comm_remove(table):
+    id_ = ui.get_inputs(["id: "], "Give me an id: ")
+    for i in range(len(table)):
+        if table[i][0] == id_[0]:
+            del table[i]
+            break
+
+
+def comm_update(table, title_list):
+    temp_list = []
+    id_ = ui.get_inputs(["id: "], "Give me an id: ")
+    new_line = []
+    new_line.append(id_[0])
+    for i in range(len(title_list)):
+        title_list[0] = title_list[i]
+        temp_list.append(ui.get_inputs(title_list, "Give me a data!"))
+        new_line.append(temp_list[i][0])
+    for i in range(len(table)):
+        if table[i][0] == id_[0]:
+            table[i] = new_line
